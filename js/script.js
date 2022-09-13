21196db7b3a41e479fa34f49fa728aea6616d580
grada92 <danielegradassai@gmail.com>
2022-09-13 17:07:50 +0200

    imposto function con click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+console.log('JS-OK')
+
 const slides = [
     {
         image: 'img/01.jpg',
@@ -28,20 +30,57 @@ const slides = [
 
 console.log(slides);
 
-const app = new Vue (
-    {
-        el: '#app',
-        methods: {
-            prevButton : function(){
-                console.log('mi hai cliccato')
+
+
+
+const app = new Vue({
+    el: '#app',
+    data:{
+        imageActive: 0,
+        images: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
             },
-
-            nextButton : function(){
-                console.log('mi hai cliccato')
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
             }
+        ]
 
+    },
+    methods:{
+        nextImage (){
+            if(this.imageActive === this.images.length - 1 ){
+                this.imageActive = 0;
+            } else{
+                this.imageActive++;
+            }
+        },
 
-
+        previousImage (){
+            if (this.imageActive === 0) {
+                this.imageActive = this.images.length - 1;
+            } else {
+                this.imageActive--;
+            }
         }
     }
-)
+})
